9451d751b485fdc7fe788fe1f1f2b070da5636f5
turkagora <turkagora@outlook.com>
2026-04-21 00:27:38 +0200

    Add files via upload

---
+++ assets/js/analytics.js
@@ -0,0 +1,11 @@
+(function () {
+  var s = document.createElement('script');
+  s.async = true;
+  s.src = 'https://www.googletagmanager.com/gtag/js?id=G-V5XXXXXXXX';
+  document.head.appendChild(s);
+
+  window.dataLayer = window.dataLayer || [];
+  window.gtag = function(){ dataLayer.push(arguments); };
+  gtag('js', new Date());
+  gtag('config', 'G-V5PTPRE7V3');
+})();
